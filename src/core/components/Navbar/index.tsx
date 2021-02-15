07743f8ece1e97a4fc64be46025a1cccb966b691
CROMER87 <mero.rjf@hotmail.com>
2021-02-15 14:52:53 -0300

    Add Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss'
+
+const Navbar = () => (
+
+    <nav className="main-nav --primary-color">
+        <a href=" " className="nav-logo-text">
+            Bootcamp DevSuperior
+        </a>
+
+    </nav>
+
+)
+
+export default Navbar;
