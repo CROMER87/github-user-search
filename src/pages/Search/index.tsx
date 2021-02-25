15a9b24096d036f5fe5644903f97de7949af606a
CROMER87 <mero.rjf@hotmail.com>
2021-02-25 16:23:56 -0300

    Add search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,8 +1,14 @@
-
+import Button from '../../core/components/Button';
+import './styles.scss';
 
 const Search = () => (
 
-    <div>Search</div>
+    <div className="search-container">
+        <h1 className="search-text">
+            Encontre um perfil Github
+        </h1>
+        <Button text="Encontrar"/>
+    </div>
 );
 
 export default Search;
